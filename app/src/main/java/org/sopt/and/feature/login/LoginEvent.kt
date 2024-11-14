6b25e424472a02077c2e5759196856da01af901c
0se0 <030ggong@gmail.com>
2024-11-15 04:40:45 +0900

    #7 Feature: Login api

--- app/src/main/java/org/sopt/and/feature/login/LoginEvent.kt
+++ app/src/main/java/org/sopt/and/feature/login/LoginEvent.kt
@@ -1,6 +1,6 @@
 package org.sopt.and.feature.login
 
 sealed class LoginEvent {
-    data class Success(val email: String) : LoginEvent()
-    data class Failure(val message: String) : LoginEvent()
+   data class Success(val token: String) : LoginEvent()
+   data class Failure(val message: String) : LoginEvent()
 }
--- app/src/main/java/org/sopt/and/feature/login/LoginViewModel.kt
+++ app/src/main/java/org/sopt/and/feature/login/LoginViewModel.kt
@@ -11,22 +11,25 @@ class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
     private val _loginEvent = MutableSharedFlow<LoginEvent>()
     val loginEvent = _loginEvent.asSharedFlow()
 
-    fun onLoginClick(email: String, password: String) {
+    fun onLoginClick(username: String, password: String) {
         viewModelScope.launch {
-            val savedEmail = userRepository.getEmail()
-            val savedPassword = userRepository.getPassword()
-
-            if (email != savedEmail) {
-                _loginEvent.emit(LoginEvent.Failure("이메일이 다릅니다."))
-                return@launch
+            runCatching {
+                userRepository.postLogin(username, password)
+            }.onSuccess { response ->
+                if (response.isSuccessful && response.body() != null) {
+                    val token = response.body()?.result?.token
+                    if (token != null) {
+                        userRepository.saveUserToken(token)
+                        _loginEvent.emit(LoginEvent.Success(token))
+                    } else {
+                        _loginEvent.emit(LoginEvent.Failure("토큰을 받아오지 못했습니다."))
+                    }
+                }  else {
+                    _loginEvent.emit(LoginEvent.Failure("로그인 실패: ${response.errorBody()?.string() ?: "알 수 없는 오류"}"))
+                }
+            }.onFailure { exception ->
+                _loginEvent.emit(LoginEvent.Failure("네트워크 오류 발생: ${exception.message}"))
             }
-
-            if (password != savedPassword) {
-                _loginEvent.emit(LoginEvent.Failure("비밀번호가 틀렸습니다."))
-                return@launch
-            }
-
-            _loginEvent.emit(LoginEvent.Success(email))
         }
     }
 }
