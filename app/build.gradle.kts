9e8ad27b46299545100fc43780f06b796086ec78
0se0 <030ggong@gmail.com>
2024-11-15 04:35:29 +0900

    #7 Chore: Network dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,14 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+}
+
+val properties = Properties().apply {
+    load(project.rootProject.file("local.properties").inputStream())
 }
 
 android {
@@ -17,6 +23,7 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "BASE_URL", properties["base.url"].toString())
     }
 
     buildTypes {
@@ -37,6 +44,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -62,6 +70,14 @@ dependencies {
     implementation(libs.androidx.ui.text)
     implementation(libs.androidx.accompanist.systemuicontroller)
     implementation(libs.androidx.compose.foundation)
-    implementation(libs.io.coil.kt )
+    implementation(libs.io.coil.kt)
+    implementation(libs.kotlinx.serialization.json)
+
+    // Network
+    implementation(platform(libs.okhttp.bom))
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.kotlin.serialization.converter)
     implementation(libs.kotlinx.serialization.json)
 }
