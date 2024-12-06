a022e66db13455a47bdfc241c488aeb1f3521a35
0se0 <030ggong@gmail.com>
2024-12-06 20:21:40 +0900

    Feature #9: Update LoginModel

--- app/src/main/java/org/sopt/and/feature/login/LoginViewModel.kt
+++ app/src/main/java/org/sopt/and/feature/login/LoginViewModel.kt
@@ -5,31 +5,23 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import org.sopt.and.data.UserRepository
+import org.sopt.and.data.repository.UserRepository
 
-class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
+class LoginViewModel(
+    private val userRepository: UserRepository
+) : ViewModel() {
     private val _loginEvent = MutableSharedFlow<LoginEvent>()
     val loginEvent = _loginEvent.asSharedFlow()
 
     fun onLoginClick(username: String, password: String) {
         viewModelScope.launch {
-            runCatching {
-                userRepository.postLogin(username, password)
-            }.onSuccess { response ->
-                if (response.isSuccessful && response.body() != null) {
-                    val token = response.body()?.result?.token
-                    if (token != null) {
-                        userRepository.saveUserToken(token)
-                        _loginEvent.emit(LoginEvent.Success(token))
-                    } else {
-                        _loginEvent.emit(LoginEvent.Failure("토큰을 받아오지 못했습니다."))
-                    }
-                }  else {
-                    _loginEvent.emit(LoginEvent.Failure("로그인 실패: ${response.errorBody()?.string() ?: "알 수 없는 오류"}"))
+            userRepository.login(username, password)
+                .onSuccess { token ->
+                    _loginEvent.emit(LoginEvent.Success(token))
                 }
-            }.onFailure { exception ->
-                _loginEvent.emit(LoginEvent.Failure("네트워크 오류 발생: ${exception.message}"))
-            }
+                .onFailure { exception ->
+                    _loginEvent.emit(LoginEvent.Failure(exception.message ?: "알 수 없는 오류가 발생했습니다."))
+                }
         }
     }
 }
